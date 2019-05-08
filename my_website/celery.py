a1e1e015f13fdc9ff987a9787fa07777d1529dce
Erin Rosenbaum <erinrosenbaum@yahoo.com>
2019-05-08 17:56:39 -0600

    venv from pipenv

--- my_website/celery.py
+++ my_website/celery.py
@@ -1,9 +1,9 @@
-# import os
-# from celery import Celery
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_website.settings')
-#
-# app = Celery('my_website')
-#
-# app.config_from_object('django.conf:settings', namespace='CELERY')
-# app.autodiscover_tasks()
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_website.settings')
+
+app = Celery('my_website')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()

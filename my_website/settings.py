7788fe2200afcd9817635f8b17e6a44d118465db
Erin Rosenbaum <erinrosenbaum@yahoo.com>
2019-05-08 21:48:36 -0600

    local settings

--- my_website/settings.py
+++ my_website/settings.py
@@ -15,14 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # Application definition
@@ -83,13 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'my_website.wsgi.application'
 
-
-
-
-
-# Password validation
-# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -105,10 +90,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/2.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 USE_I18N = True
@@ -116,10 +97,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = False
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
 
@@ -144,7 +121,6 @@ AUTHENTICATION_BACKENDS = [
 #     BRAINTREE_PUBLIC_KEY,
 #     BRAINTREE_PRIVATE_KEY
 # )
-
 
 THUMBNAIL_DEBUG = True
 

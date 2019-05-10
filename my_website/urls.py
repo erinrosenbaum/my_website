d9c924bd56bfe7340cd23f50bef310d433b36dbb
Erin Rosenbaum <erinrosenbaum@yahoo.com>
2019-05-09 21:53:50 -0600

    media files

--- my_website/urls.py
+++ my_website/urls.py
@@ -39,5 +39,5 @@ urlpatterns = [
     # path('jobs/', jobs.views.jobs_home, name='jobs_home'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

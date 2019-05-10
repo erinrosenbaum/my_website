3b0ff9707ebcc7250712fe94937c7cb3bc0d83ae
Erin Rosenbaum <erinrosenbaum@yahoo.com>
2019-05-09 20:43:13 -0600

    media files

--- my_website/settings.py
+++ my_website/settings.py
@@ -105,6 +105,9 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
@@ -129,8 +132,6 @@ AUTHENTICATION_BACKENDS = [
 
 THUMBNAIL_DEBUG = True
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CART_SESSION_ID = 'cart'
 
 from braintree import Configuration, Environment
